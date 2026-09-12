00795046549f511c52f3198d0c8f662b1f0e8760
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-12 14:06:43 -0700

    Seventh commit

--- comsc-210-lab6.cpp
+++ comsc-210-lab6.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 
+//Function Prototypes
 void enterArrayData(double* arr, int SIZE);
 void outputArrayData(double* arr, int SIZE);
 double sumArray(double* arr, int SIZE);
@@ -19,7 +20,7 @@ int main(){
 
     outputArrayData(arr, SIZE);
 
-    sumArray(arr, SIZE);
+    cout << "Sum: " << sumArray(arr, SIZE) << endl;
 
     delete [] arr;
 
@@ -27,34 +28,43 @@ int main(){
 
     return 0;
 
-
 }
 
+//Takes in array and size
+//Iterates using for loop
+//Inputs values using pointer notation
 void enterArrayData(double* arr, int SIZE){
     double val;
 
     cout << "Data entry for the array: " << endl;
     for(int i = 0; i < SIZE; i++){
-        cout << "> Enter Double For Element #" << i + 1 << endl;
+        cout << "> Enter Double For Element # " << i + 1 << " ";
         cin >> val;
         *(arr + i) = val;
-        arr++;   
     }
 
     cout << "Data Entry Complete." << endl;
 
 }
 
+//Takes in array and size
+//iterates through for loop
+//Outputs elements
 void outputArrayData(double* arr, int SIZE){
 
     cout << "Outputting array elements: " << endl;
 
     for(int i = 0; i < SIZE; i++){
-        cout << arr[i] << " ";
+        cout << *(arr + i) << " ";
     }
+
+    cout << endl;
 
 }
 
+//Takes in arr and SIZE
+//Adds each value to sum
+//Returns sum to main
 double sumArray(double* arr, int SIZE){
     double sum = 0;
 
@@ -63,5 +73,5 @@ double sumArray(double* arr, int SIZE){
     }
 
     return sum;
-    
+
 }
