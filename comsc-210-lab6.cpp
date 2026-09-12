d38d055e2a31c9ab645656cce9e05939bd0ca879
ahmeddharhan2255-hub <ahmeddharhan2255@gmail.com>
2026-09-12 12:37:41 -0700

    Second commit

---
+++ comsc-210-lab6.cpp
@@ -0,0 +1,15 @@
+// COMSC-210 | Lab 6 | Ahmad Dharhan
+
+#include <iostream>
+
+using namespace std;
+
+void enterArrayData();
+void outputArrayData();
+void sumArray();
+
+const int SIZE = 5;
+
+int main(){
+
+}
